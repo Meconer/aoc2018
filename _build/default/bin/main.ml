8dcbaaad894a3ca4114c3c456bf7c759ba355704
Meconer <mats.asson@gmail.com>
2025-01-07 12:23:18 +0100

    Day 21

--- _build/default/bin/main.ml
+++
@@ -1,7 +0,0 @@
-let () =
-  Printf.printf "Day16 part1: %d\n" Day16.resultP1;
-  Printf.printf "Day16 part2: %d\n" Day16.resultP2;
-  Printf.printf "Day19 part1: %d\n" Day19.resultP1;
-  Printf.printf "Day19 part2: %d\n" Day19.resultP2;
-  Printf.printf "Day20 part1: %d\n" Day20.resultP1;
-  Printf.printf "Day20 part2: %d\n" Day20.resultP2
